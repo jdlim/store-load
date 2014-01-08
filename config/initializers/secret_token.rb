6c79d302b7b769843cb5adf3380a7f8824ff2f0a
jdlim <jlim@8HPZQW1.alexanderwang.local>
2014-01-08 13:25:56 -0500

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StoreLoad::Application.config.secret_key_base = '71d26022135bc2d07e79f4c57eed942f49665e571ef749ef45974b3fa58e3db5b58feda5a9cdd153105cce5c09037c794c43b45ca0767b021a0e64cafc99ab9a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StoreLoad::Application.config.session_store :cookie_store, key: '_store-load_session'
